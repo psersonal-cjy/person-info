befa1b5342638f83724eadd7ddf41631d21239db
zhaozy <399050934@qq.com>
2020-04-04 23:30:19 +0800

    first commit

---
+++ src/test/java/com/management/personinfo/PersonInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.management.personinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
